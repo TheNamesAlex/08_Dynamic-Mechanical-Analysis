0411aa784e3845221045b107cf019725cfedb747
Alexander Resch <alexander.resch@aabilon.de>
2021-09-25 12:34:53 +0200

    GUI added raw / normalized plot + refactoring

--- GUI/demo.py
+++ GUI/demo.py
@@ -7,12 +7,20 @@ from PandasConvert import PandasModel
 import pandas as pd
 import numpy as np
 
-pg.setConfigOption('background', 'w')
-pg.setConfigOption('foreground', 'k')
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
 
-#design = pygraph.Ui_MainWindow
+
+import matplotlib
+matplotlib.use('Qt5Agg')
+
+pg.setConfigOption('background', 'w')
+#pg.setConfigOption('foreground', 'k')
+
 
 class MainWindow(QtWidgets.QMainWindow):
+
+    data_in_plot = False
 
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
@@ -30,57 +38,98 @@ class MainWindow(QtWidgets.QMainWindow):
         #connect "print raw data"-button to corresp. method
         self.printRawData.clicked.connect(self.plot_raw_data)
 
+        # connect "print raw data"-button to corresp. method
+        self.printNormalizedData.clicked.connect(self.plot_normalized_data)
 
-    def plot(self, x, y):
+
+    def plot(self, x, y, pen):
+        self.graphWidget.plot(x, y, pen=pen)
+
+    def clear_plot(self):
         self.graphWidget.clear()
-        self.graphWidget.plot(x, y)
+        self.data_in_plot = False
+
+    def update_data(self):
+        # populate table after loading data
+        self.names = ['Point', 'Time_Elapsed', 'Time_Scan', 'Displacement', 'Load', 'E11', 'E12', 'E22', 'Ax_cmd',
+                      'Ax_err', 'No_Val']
+
+        self.data = pd.read_csv(self.filepath, header=2, names=self.names)
+
+        # remove duplicate rows
+        self.data = self.data.drop_duplicates(subset=['Time_Elapsed'], keep='first')
+
+        # assign walking index
+        self.data['Index'] = [i for i in range(1, len(self.data) + 1)]
+
+        # add normalized data
+        self.data['Displacement_normalized'] = self.data['Displacement'] / max(self.data['Displacement'])
+        self.data['Load_normalized'] = self.data['Load'] / max(self.data['Load'])
+
+        # print data to table
+        self.model = PandasModel(self.data)
+        self.tableView.setModel(self.model)
+
+        # after loading new data, clear the plot
+        self.clear_plot()
 
     def fileSelect(self):
-        # Importing global variables
 
-        global filepath, forceColumn, displacementColumn
+        #init filepath
+        self.filepath = ''
 
         # Open a new window to select files - only .mat files will be shown
         dialog = QtGui.QFileDialog(None, "Open File", None, "CSV-files (*.csv)")
         dialog.exec_()
 
         for file in dialog.selectedFiles():
-            filepath = file
+            self.filepath = file
 
-        #populate table after loading data
-        self.names =  ['Point','Time_Elapsed','Time_Scan','Displacement','Load','E11','E12','E22','Ax_cmd','Ax_err','No_Val']
-        self.data = pd.read_csv(filepath,header=2,names=self.names)
-
-        #print data to table
-        model = PandasModel(self.data)
-        self.tableView.setModel(model)
+        self.update_data()
 
 
     def plot_raw_data(self):
 
-        # update column names
-        forceColumn = int(self.forceColumn.toPlainText())
-        displacementColumn = int(self.displacementColumn.toPlainText())
+        if self.data_in_plot:
+            self.clear_plot()
 
-        print(self.names[forceColumn], self.names[displacementColumn])
+        Load = self.data['Load'].values
+        Displacement = self.data['Displacement'].values
+        Index = self.data['Index'].values
 
-        #forceTag = self.data['Displacement']
-        #displacementTag = self.data.columns['Displacement']
+        pen = pg.mkPen(color=(255,136,0), width=1, alpha=0.25)
+        self.plot(Index,Displacement,pen)
 
-        y = self.data['Load'].values
-        x = self.data['Displacement'].values
+        pen = pg.mkPen(color=(200, 200, 255), width=1, alpha=0.25)
+        self.plot(Index,Load,pen)
 
-        #print(forceTag,displacementTag)
-        print(y,x)
+        self.data_in_plot = True
 
-        self.plot(x,y)
+    def plot_normalized_data(self):
+
+        if self.data_in_plot:
+            self.clear_plot()
+
+        Load = self.data['Load_normalized'].values
+        Displacement = self.data['Displacement_normalized'].values
+        Index = self.data['Index'].values
+
+        pen = pg.mkPen(color=(255,136,0), width=1, alpha=0.25)
+        self.plot(Index,Displacement,pen)
+
+        pen = pg.mkPen(color=(200, 200, 255), width=1, alpha=0.25)
+        self.plot(Index,Load,pen)
+
+        self.data_in_plot = True
+
+
 
 
 def main():
 
-    global filepath
-    global forceColumn
-    global displacementColumn
+    #global filepath
+    #global forceColumn
+    #global displacementColumn
 
     app = QtWidgets.QApplication(sys.argv)
     main = MainWindow()
